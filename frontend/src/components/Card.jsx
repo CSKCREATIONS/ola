162b0081af25858d33f9fe50a2c33d137604db0c
CSKCREATIONS <juliandavicast@gmail.com>
2025-11-12 02:21:33 -0500

    cambios en Reliability

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export function Card({ children, className = '' }) {
   return (
@@ -15,3 +16,12 @@ export function CardContent({ children, className = '' }) {
     </div>
   );
 }
+Card.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string
+};
+
+CardContent.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string
+};
